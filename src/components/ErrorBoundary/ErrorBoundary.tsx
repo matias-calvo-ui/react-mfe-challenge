c3a9ddf54082e0e395136ce9f3f649807cbee2b4
Matias <matias.marcelo.calvo@gmail.com>
2024-06-04 17:20:44 +0200

    feat: ErrorBoundary component created

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,30 @@
+import React, { Component, ErrorInfo } from "react";
+import {
+  ErrorBoundaryProps,
+  ErrorBoundaryState,
+} from "../../types/ErrorBoundary";
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Error caught in ErrorBoundary: ", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
